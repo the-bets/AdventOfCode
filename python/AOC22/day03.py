48704267be0eb21c7068dd9db0ed9474419b0edc
ebbehun <87673759+the-bets@users.noreply.github.com>
2022-12-06 12:16:23 -0600

    day 3 part 2
    I could make this better but I'm tired of looking at the problem ¯\_(ツ)_/¯

--- python/AOC22/day03.py
+++ python/AOC22/day03.py
@@ -1,15 +1,34 @@
 import string
 
 
-def findPriority(a: set, b: set) -> int:
-    prio = int(string.ascii_letters.index(
+def priorityPartOne(a: set, b: set) -> int:
+    prio = 0
+    prio += int(string.ascii_letters.index(
         "".join(set(a).intersection(b)))) + 1
     return prio
 
 
-with open("inputs/day03.txt", "r") as f:
+def one(l: list) -> int:
     prio = 0
-    for l in f:
-        a, b = l[:len(l) // 2], l[len(l) // 2:]
-        prio += findPriority(a, b)
-    print(f"Part One: Priority is {prio}")
+    for i in l:
+        b, c = i[:len(i) // 2], i[len(i) // 2:]
+        prio += priorityPartOne(b, c)
+    return prio
+
+
+def two(l: list) -> int:
+    prio = 0
+    for i in range(len(l) // 3):
+        match = set(string.ascii_letters)
+        for item in l[i*3:i*3+3]:
+            match = match.intersection(set(item))
+        prio += string.ascii_letters.index(next(iter(match))) + 1
+    return prio
+
+
+base = set(string.ascii_letters)
+with open("inputs/day03.txt") as f:
+    l = f.read().splitlines()
+one = one(l)
+two = two(l)
+print(f"Part One: {one}, Part Two: {two}")
